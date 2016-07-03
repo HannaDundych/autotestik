61826b30b41d4ef6f5efd75be8a7ca918f31d4e4
Hanna <hdu@ciklum.com>
2016-07-03 20:27:13 +0300

    reduce unused variables, name change

--- test_registration.rb
+++ test_registration.rb
@@ -26,9 +26,9 @@ class TestRegistration < Test::Unit::TestCase
 
 
   def test_positive_login
-    createdLogin = register_user
+    created_login = register_user
     logout_user
-    login_user createdLogin
+    login_user created_login
 
     expected_text = "My account"
     actual_text = @driver.find_element(:class, 'my-account').text
@@ -36,8 +36,8 @@ class TestRegistration < Test::Unit::TestCase
   end
 
   def test_create_proj
-    createdLogin = register_user
-    createdProject = create_new_proj
+    register_user
+    create_new_proj
     expected_text = "Successful creation."
     actual_text = @driver.find_element(:id, 'flash_notice').text
     assert_equal(expected_text, actual_text)
@@ -45,49 +45,49 @@ class TestRegistration < Test::Unit::TestCase
   end
 
   def test_create_project_version
-    createdLogin = register_user
-    createdProject = create_new_proj
+    register_user
+    create_new_proj
 
-    numberVersionsBefore = @driver.find_elements(:class, 'version').length
+    number_versions_before = @driver.find_elements(:class, 'version').length
     create_project_version 'version1'
     sleep 3
-    numberVersionsAfter = @driver.find_elements(:class, 'version').length
-    assert_equal(numberVersionsBefore + 1, numberVersionsAfter)
+    number_versions_after = @driver.find_elements(:class, 'version').length
+    assert_equal(number_versions_before + 1, number_versions_after)
 
 
   end
 
   def test_create_issues
-    createdLogin = register_user
-    createdProject = create_new_proj
+    register_user
+    create_new_proj
     @driver.find_element(:class, 'issues').click
-    numberIssuesBefore = @driver.find_elements(:class, 'issue').length
+    number_issues_before = @driver.find_elements(:class, 'issue').length
 
     create_issue 'bug', 'subject1'
     create_issue 'feature', 'subject2'
     create_issue 'support', 'subject3'
 
     @driver.find_element(:class, 'issues').click
-    numberIssuesAfter = @driver.find_elements(:class, 'issue').length
-    assert_equal(numberIssuesBefore + 3, numberIssuesAfter)
+    number_issues_after = @driver.find_elements(:class, 'issue').length
+    assert_equal(number_issues_before + 3, number_issues_after)
 
 
   end
 
   def test_add_user_to_project
-    createdLogin1 = register_user
+    created_login = register_user
     logout_user
-    createdLogin2 = register_user
-    createdProject = create_new_proj
-    numbersActiveUsersBefore = @driver.find_elements(:class, 'active').length
-    add_user_to_project createdLogin1
+    register_user
+    create_new_proj
+    numbers_active_users_before = @driver.find_elements(:class, 'active').length
+    add_user_to_project created_login
     sleep 3
-    numbersActiveUsersAfter = @driver.find_elements(:class, 'active').length
-    assert_equal(numbersActiveUsersBefore + 1, numbersActiveUsersAfter)
+    numbers_active_users_after = @driver.find_elements(:class, 'active').length
+    assert_equal(numbers_active_users_before + 1, numbers_active_users_after)
   end
 
   def test_positive_logout
-    createdLogin = register_user
+    register_user
     logout_user
 
     expected_text = "Sign in"
@@ -97,7 +97,7 @@ class TestRegistration < Test::Unit::TestCase
 
 
   def test_change_password
-    createdLogin = register_user
+    register_user
     change_password 'qwerty', 'qwerty1'
     expected_text = "Password was successfully updated."
     actual_text = @driver.find_element(:id, 'flash_notice').text
@@ -136,68 +136,68 @@ class TestRegistration < Test::Unit::TestCase
 
   def logout_user
     @driver.navigate.to @url
-    logoutButtons = @driver.find_elements(:class, 'logout')
-    if (logoutButtons.empty?)
+    logout_buttons = @driver.find_elements(:class, 'logout')
+    if logout_buttons.empty?
     else
-      logoutButtons.first.click
+      logout_buttons.first.click
     end
   end
 
   def create_new_proj
-    projectName = ('name'+ rand(9999).to_s)
+    project_name = ('name'+ rand(9999).to_s)
     @driver.find_element(:class, 'projects').click
     @driver.find_element(:class, 'icon-add').click
-    @driver.find_element(:id, 'project_name').send_keys projectName
+    @driver.find_element(:id, 'project_name').send_keys project_name
     @driver.find_element(:id, 'project_description').send_keys '1234'
-    @driver.find_element(:id, 'project_identifier').send_keys projectName
+    @driver.find_element(:id, 'project_identifier').send_keys project_name
     @driver.find_element(:name, 'commit').click
-    return projectName
+    return project_name
   end
 
-  def create_project_version versionName
+  def create_project_version (version_name)
     @driver.find_element(:id, 'tab-versions').click
     sleep 2
     @driver.find_elements(:class, 'icon-add')[1].click
     sleep 5
-    @driver.find_element(:id, 'version_name').send_keys versionName
+    @driver.find_element(:id, 'version_name').send_keys version_name
     @driver.find_element(:name, 'commit').click
   end
 
-  def create_issue issueTypeName, subject
+  def create_issue (issue_type_name, subject)
     @driver.find_element(:class, 'new-issue').click
     sleep 2
     @driver.find_element(:id, 'issue_tracker_id').click
-    @driver.find_element(:id, 'issue_tracker_id').send_keys [issueTypeName[0], :enter]
+    @driver.find_element(:id, 'issue_tracker_id').send_keys [issue_type_name[0], :enter]
     sleep 2
     @driver.find_element(:id, 'issue_subject').send_keys subject
     @driver.find_element(:name, 'commit').click
   end
 
 
-  def change_password oldPassword, newPassword
+  def change_password (old_password, new_password)
 
     @driver.find_element(:class, 'my-account').click
     @driver.find_element(:class, 'icon-passwd').click
-    @driver.find_element(:id, 'password').send_keys oldPassword
-    @driver.find_element(:id, 'new_password').send_keys newPassword
-    @driver.find_element(:id, 'new_password_confirmation').send_keys newPassword
+    @driver.find_element(:id, 'password').send_keys old_password
+    @driver.find_element(:id, 'new_password').send_keys new_password
+    @driver.find_element(:id, 'new_password_confirmation').send_keys new_password
     @driver.find_element(:name, 'commit').click
 
   end
 
 
-  def add_user_to_project userName
+  def add_user_to_project (user_name)
 
     @driver.find_element(:id, 'tab-members').click
     @driver.find_element(:class, 'icon-add').click
     sleep 5
-    @driver.find_element(:id, 'principal_search').send_keys userName
+    @driver.find_element(:id, 'principal_search').send_keys user_name
     sleep 5
-    usersList = @driver.find_elements(:name, 'membership[user_ids][]')
-    assert usersList.length==1
-    usersList.first.click
-    rolesList = @driver.find_elements(:name, 'membership[role_ids][]')
-    rolesList[4].click
+    users_list = @driver.find_elements(:name, 'membership[user_ids][]')
+    assert users_list.length==1
+    users_list.first.click
+    roles_list = @driver.find_elements(:name, 'membership[role_ids][]')
+    roles_list[4].click
     @driver.find_element(:id, 'member-add-submit').click
 
   end
